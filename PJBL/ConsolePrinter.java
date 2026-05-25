ffb6cacbf815a4a14b9b8ae28f7d9b5cbb64f409
Rizky Fadliansyah <blogerrizky@gmail.com>
2026-05-25 21:56:20 +0700

    Delete PJBL directory

--- PJBL/ConsolePrinter.java
+++
@@ -1,9 +0,0 @@
-package PJBL;
-
-public class ConsolePrinter implements Printer {
-    public void print(Ulasan u) {
-        System.out.println("Nama         : " + u.nama);
-        System.out.println("Isi Ulasan   : " + u.isi);
-        System.out.println("Tanggal      : " + u.tanggal);
-    }
-}
