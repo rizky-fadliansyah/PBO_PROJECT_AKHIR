9c030447ea78fc46207ae223d8dbda992744fa9b
redtegar <redtegar123@gmail.com>
2026-05-26 14:29:05 +0700

    crud|UlasanService

--- Project_akhirPBO/UlasanService.java
+++ Project_akhirPBO/UlasanService.java
@@ -1,14 +1,45 @@
-public class UlasanService {
+import java.util.ArrayList;
 
-    public void tambahUlasan() {
+public class UlasanService {
+    private ArrayList<Ulasan> daftarUlasan = new ArrayList<>();
+
+    public void tambahUlasan(Ulasan ulasan) {
+        daftarUlasan.add(ulasan);
         System.out.println("Ulasan ditambahkan");
     }
 
-    public void hapusUlasan() {
-        System.out.println("Ulasan dihapus");
+    public void hapusUlasan(int id) {
+        for (Ulasan u : daftarUlasan) {
+            if (u.getIdUlasan() == id) {
+                daftarUlasan.remove(u);
+                System.out.println("Ulasan dihapus");
+                return;
+            }
+        }
+        System.out.println("Ulasan tidak ditemukan");
     }
 
     public void lihatUlasan() {
-        System.out.println("Menampilkan ulasan");
+        if (daftarUlasan.isEmpty()) {
+            System.out.println("Belum ada ulasan");
+            return;
+        }
+
+        for (Ulasan u : daftarUlasan) {
+            u.menampilkanUlasan();
+            System.out.println("====================");
+        }
+    }
+
+    public void updateUlasan(int id, String komentarBaru, int ratingBaru) {
+        for (Ulasan u : daftarUlasan) {
+            if (u.getIdUlasan() == id) {
+                u.setKomentar(komentarBaru);
+                u.setRating(ratingBaru);
+                System.out.println("Ulasan diupdate");
+                return;
+            }
+        }
+        System.out.println("Ulasan tidak ditemukan");
     }
 }
